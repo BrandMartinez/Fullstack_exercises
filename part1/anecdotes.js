21f5bde9c932b08012d9de51de08ffbeef848a97
BrandMartinez <39375127+BrandMartinez@users.noreply.github.com>
2021-10-04 12:14:44 +0200

    Exercise 1.14

--- part1/anecdotes.js
+++ part1/anecdotes.js
@@ -10,9 +10,19 @@ const Button = (props) => (
   </div>
 )
 
-const setVotes = (points,setPoints,selected) => {
-  const copy = { ...points }
+const setVotes = (points,setPoints,selected,setVoted) => {
+  const copy = [ ...points ]
   copy[selected] += 1
+  let i = 0
+  let j = 0
+  copy.forEach(value => {
+    if(value>copy[i])
+    {
+      i=j
+    }
+    j++
+  })
+  setVoted(i)
   setPoints(copy)
 }
 
@@ -21,17 +31,36 @@ const setVotes = (points,setPoints,selected) => {
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [voted, setVoted] = useState(5)
   const [points, setPoints] = useState([1, 4, 6, 3, 5, 7])
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{props.anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
       <Button handleClick={() => setSelected(Math.floor(Math.random() * 6))} text="next anecdote" />
-      <Button handleClick={() => setVotes(points,setPoints,selected)} text="vote" />
+      <Button handleClick={() => setVotes(points,setPoints,selected,setVoted)} text="vote" />
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[voted]}</p>
+      <p>has {points[voted]} votes</p>
     </div>
   )
 }
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
 
 const anecdotes = [
   'If it hurts, do it more often',
